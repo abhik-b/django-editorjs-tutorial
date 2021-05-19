3e8e9dbc3faccb04d0bb7cd53166e07a209a16c5
abhik-b <abhik0301@gmail.com>
2021-05-19 16:06:04 +0530

    basic features added 🚀💯

---
+++ tutorial/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django_editorjs import EditorJsField
+# Create your models here.
+
+class Post(models.Model):
+    title=models.CharField(max_length=60)
+    body=EditorJsField(
+        editorjs_config={
+            "tools":{
+                "Link":{
+                    "config":{
+                        "endpoint":
+                            '/linkfetching/'
+                        }
+                },
+                "Image":{
+                    "config":{
+                        "endpoints":{
+                            "byFile":'/uploadi/',
+                            "byUrl":'/uploadi/'
+                        },
+                       
+                    }
+                },
+                "Attaches":{
+                    "config":{
+                        "endpoint":'/uploadf/'
+                    }
+                }
+            }
+        }
+    )
---
+++ tutorial/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Post
+from django.contrib import messages
+from django.forms import ModelForm
+from django.http import JsonResponse
+from django.core.files.storage import FileSystemStorage
+from django.views.decorators.csrf import requires_csrf_token
+# Create your views here.
+
+class PostForm(ModelForm):
+    class Meta:
+        model=Post
+        fields=['title','body']
+
+
+def home(request):
+    if request.method=="POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post=form.save()
+            messages.success(request,'submitted succesfully {}'.format(post))
+            return redirect('/')      
+    form=PostForm()
+    return render(request,'tutorial/index.html',{'form':form})
+
+
+
+def postdetail(request,id):
+    post=Post.objects.get(id=id)
+    return render(request,'tutorial/detail.html',{'post':post})
+
+
+@requires_csrf_token
+def uploadi(request):
+    f=request.FILES['image']
+    fs=FileSystemStorage()
+    filename=str(f).split('.')[0]
+    file= fs.save(filename,f)
+    fileurl=fs.url(file)
+    return JsonResponse({'success':1,'file':{'url':fileurl}})
+
+@requires_csrf_token
+def uploadf(request):
+        f=request.FILES['file']
+        fs=FileSystemStorage()
+        filename,ext=str(f).split('.')
+        print(filename,ext)
+        file=fs.save(str(f),f)
+        fileurl=fs.url(file)
+        fileSize=fs.size(file)
+        return JsonResponse({'success':1,'file':{'url':fileurl,'name':str(f),'size':fileSize}})
+
+
+def upload_link_view(request):
+    import requests
+    from bs4 import BeautifulSoup  
+
+    print(request.GET['url'])
+    url = request.GET['url']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,features="html.parser")
+    metas = soup.find_all('meta')
+    description=""
+    title=""
+    image=""
+    for meta in metas:
+        if 'property' in meta.attrs:
+            if (meta.attrs['property']=='og:image'):
+                image=meta.attrs['content']         
+        elif 'name' in meta.attrs:         
+            if (meta.attrs['name']=='description'):
+                description=meta.attrs['content']
+            if (meta.attrs['name']=='title'):
+                title=meta.attrs['content']
+    return JsonResponse({'success':1,'meta':
+    {"description":description,"title":title, "image":{"url":image}
+        }})
